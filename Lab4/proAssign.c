e421e798bf9fd3fa2b01585fdf4015c31f0218ec
George <ebelingg@mail.gvsu.edu>
2021-02-15 16:33:22 -0500

    Added polling

--- Lab4/proAssign.c
+++ Lab4/proAssign.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <signal.h>
 #include <time.h>
+#include <poll.h>
 
 #define NUM_THREADS 10
 
@@ -22,35 +23,42 @@ int main() {
     srand(time(0)); //give a seed to rand
 
     signal(SIGINT, sig_handler_parent);  //Register the signal handler
-    pthread_t thread[NUM_THREADS];
+    pthread_t thread[NUM_THREADS]; //Create array of usable threads to keep track of
+
+    int index = 0, requests = 0;
+
+    struct pollfd scanner = {STDIN_FILENO, POLLIN|POLLPRI};
 
 
-// create and start two threads executing the "workerFunction" function
-// pass each thread a pointer to its respective argument
-
-int index, requests;
+    printf("Please enter file name:");
 
     while (1) { //This is going to need to have a break condition
         char fileName[20]; //Hopefully, this can be a local variable. If there is undefined behavior occuring in the worker thread, this being deallocated and reallocated as a new filename might be the cause.
 
-        if(CheckTermination(0)) break; //If marked for termination, exit the loop
+        fflush(stdout);
 
-        /// Get the file name from the user
-        printf("Please enter file name: "); fflush(stdout);
-        scanf("%s", fileName);
-        requests++; //Another request was received
+        if(poll(&scanner, 1, 100)) {
+            /// Get the file name from the user
+            scanf("%s", fileName); // Blocking
+            requests++; // Another request was received
 
-        /// Create a worker thread
-        if ((status = pthread_create(&thread[index], NULL, workerFunction, &fileName)) != 0) {
-            fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
-            exit(1);
+            /// Create a worker thread
+            if ((status = pthread_create(&thread[index], NULL, workerFunction, &fileName)) != 0) {
+                fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
+                exit(1);
+            }
+            index = (index > 9) ? 0 : index + 1; // Increase index by 1 and reset to 0 once over 9
+
+            printf("Please enter file name:");
         }
-        index = (index > 9) ? 0 : index + 1; //increase index by 1 and reset to 0 once over 9
+        else{
+            if(CheckTermination(0)) break;
+        }
 }
     printf("Waiting for worker threads to terminate...\n"); // Show that the program is closing
 
-    // join with the threads (wait for them to terminate);
-    for(int i = 0; i <= 9; i++) {
+    // Join with the threads (wait for them to terminate);
+    for(int i = 0; i <= NUM_THREADS - 1; i++) {
         if ((status = pthread_join(thread[i], &thread[i])) != 0) {
             fprintf(stderr, "join error %d: %s\n", status, strerror(status));
             exit(1);
@@ -62,20 +70,22 @@ int index, requests;
 
 int CheckTermination(int term) {
     static int terminate = 0;
+    printf("\nterminate: %d\n", terminate);
     if(term != 0) {
         terminate = 1;
+        printf("\nterminate: %d\n", terminate);
     }
     return terminate;
 }
+
 /* Signal Handler for the Parent Process */
 void sig_handler_parent(int signum) {
-
+    printf("\ngot a thing\n");
    CheckTermination(1);
 }
 
 void* workerFunction (void* arg)
 {
-// note the cast of the void pointer to the desired data type
     char *val_ptr = (char *) arg;
     int quick = 8;
     int slow = 10;
@@ -95,7 +105,6 @@ void* workerFunction (void* arg)
     }
     printf("Worker thread got %s\n", val_ptr);
 
-
-// print out a message
+    // Thread has completed and can exit cleanly
     pthread_exit(0);
-} 
+}
