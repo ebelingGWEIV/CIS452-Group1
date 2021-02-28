be493ec0d4b2f9207ecc8063b5e6bb4bbec7429d
George <ebelingg@mail.gvsu.edu>
2021-02-28 18:16:46 -0500

    Pushing before I go to save my work. Untested, but I added the actualy semaphores.

--- Lab6/Semaphore.h
+++ Lab6/Semaphore.h
@@ -11,16 +11,16 @@ struct semaphore{
 
 
 
-void Wait(struct  semaphore *);
-void Signal(struct semaphore *);
-struct semaphore compare_and_swap(struct semaphore *, int, int);
+void Wait(struct  mySemaphore *);
+void Signal(struct mySemaphore *);
+struct mySemaphore1 compare_and_swap(struct mySemaphore1 *, int, int);
 
 /*
  *
  */
-struct semaphore compare_and_swap(struct  semaphore *value, int expected, int new_value)
+struct mySemaphore1 compare_and_swap(struct  mySemaphore1 *value, int expected, int new_value)
 {
-    struct semaphore temp = *value;
+    struct mySemaphore1 temp = *value;
     if(value->val == expected)
         value->val = new_value;
     return temp;
@@ -29,7 +29,7 @@ struct semaphore compare_and_swap(struct  semaphore *value, int expected, int ne
 /**
  * Wait until a spot is available.
  */
-void Wait(struct semaphore *sema)
+void Wait(struct mySemaphore1 *sema)
 {
     while (compare_and_swap(sema, 1, 0).val < 1)
         ;
@@ -39,7 +39,7 @@ void Wait(struct semaphore *sema)
 /**
  * Signal that the spot is available again.
  */
-void Signal(struct semaphore *sema)
+void Signal(struct mySemaphore1 *sema)
 {
     while(compare_and_swap(sema, 0, 1).val > 0);
     sema->nextCall = Wait;
--- Lab6/sample1.c
+++ Lab6/sample1.c
@@ -7,21 +7,26 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include "Semaphore.h"
+#include <fcntl.h>
+
 
 #define SIZE 16
 
 void OpenSharedMem(long **shmPtr, int *shmId);
 void CloseSharedMem(const long *shmPtr, int shmId);
-void Swap(long temp, long *shmPtr, struct semaphore *sema);
+void Swap(long temp, long *shmPtr);
 
-sem_t *semaphore = NULL;
+sem_t *mySemaphore1 = NULL;
+#define MY_SEM_1 "/mySem1"
+sem_t *mySemaphore2 = NULL;
+#define MY_SEM_2 "/mySem2"
 
 int main (int argc, char *argv[])
 {
     int status;
     long int i, loop, temp = 0;
-    long int *shmPtr, *semPtr;
-    int shmId, semId;
+    long int *shmPtr;
+    int shmId;
     pid_t pid;
 
     // get value of loop variable (from command-line argument)
@@ -30,29 +35,39 @@ int main (int argc, char *argv[])
     }
 
     OpenSharedMem(&shmPtr, &shmId);
-    OpenSharedMem(&semPtr, &semId);
 
     shmPtr[0] = 0;
     shmPtr[1] = 1;
-    struct semaphore *sema = (struct semaphore *)semPtr;
-    sema->val = 1;
-    sema->nextCall = Wait;
+
+    if(mySemaphore1 = sem_open(MY_SEM_1, O_CREAT | O_EXCL, 0666, SHM_LOCKED))
+    {
+        printf("error creating semaphore\n");
+    }
+    if(mySemaphore2 = sem_open(MY_SEM_2, O_CREAT | O_EXCL, 0666, SHM_LOCKED))
+    {
+        printf("error creating semaphore\n");
+    }
 
     if (!(pid = fork())) {
         for (i=0; i<loop; i++) {
             // swap the contents of shmPtr[0] and shmPtr[1]
-            Swap(temp, shmPtr, sema);
+            sem_wait(mySemaphore1);
+            Swap(temp, shmPtr);
+            sem_post(mySemaphore2);
         }
         if (shmdt (shmPtr) < 0) {
             perror ("just can't let go\n");
             exit (1);
         }
+        sem_wait(mySemaphore1); //wait one last time to let the parent end cleanly.
         exit(0);
     }
     else {
         for (i = 0; i < loop; i++) {
             // swap the contents of shmPtr[1] and shmPtr[0]
-            Swap(temp, shmPtr, sema);
+            sem_post(mySemaphore2);
+            sem_wait(mySemaphore1);
+            Swap(temp, shmPtr);
         }
     }
 
@@ -60,18 +75,20 @@ int main (int argc, char *argv[])
     printf ("values: %li\t%li\n", shmPtr[0], shmPtr[1]);
 
     CloseSharedMem(shmPtr, shmId);
-    CloseSharedMem(semPtr, semId);
+
+    sem_post(mySemaphore1);
+    sem_close(mySemaphore2);
+    sem_close(mySemaphore1);
+    sem_unlink(MY_SEM_1);
+    sem_unlink(MY_SEM_2);
 
     return 0;
 }
 
-void Swap(long temp, long *shmPtr, struct semaphore *sema) {
-    sema->nextCall(sema);
-    static int amount = 0;
+void Swap(long temp, long *shmPtr) {
     temp = shmPtr[0];
     shmPtr[0] = shmPtr[1];
     shmPtr[1] = temp;
-    sema->nextCall(sema);
 }
 
 void CloseSharedMem(const long *shmPtr, int shmId) {
