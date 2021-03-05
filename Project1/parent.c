88e87070df2e8fcecd4b2656983c4b7c3ccc77db
George <ebelingg@mail.gvsu.edu>
2021-03-05 16:32:15 -0500

    Made pipes without error

--- Project1/parent.c
+++ Project1/parent.c
@@ -7,41 +7,63 @@
 #include <string.h>
 #include <errno.h>
 
-void Execute(char * command[]);
+#define READ 0
+#define WRITE 1
+
+id_t Execute(char * command[], int procNum, int nextNum);
 
 void StartChildren(int numProc);
+
+id_t *childID;
+
+
+int fd[20][2];
+void InitPipes(int numProc)
+{
+    for(int index = 0; index < numProc; index++)
+    {
+        if (pipe (fd[index]) < 0) {
+            perror ("plumbing problem");
+            printf("index %d\n", index);
+            exit(1);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     int numProc;
-    pid_t child;
     if(argc == 2) {
         numProc = atoi(argv[1]);
     }
     else
     {
         printf("Expected an argument for the number of processes to use\n");
-        return 1;
+        exit(1);
     }
     if(numProc <= 1) {
         printf("Number of processes must be more than 1\n");
-        return 1;
+        exit(1);
     }
+    childID = malloc((numProc+1) * sizeof(id_t));
+
+    InitPipes(numProc);
     StartChildren(numProc);
+    free(childID);
 }
 
 void StartChildren(int numProc) {//CLion needs a full path               // Pointer to the file name
 
-    //Fork to create all of the processes, attach pipes to each process
     for(int index = 1; index < numProc; index++) //Start at index == 1 because we already have the parent process
     {
             char *args[256];            // Array of pointers for each argument
             char childVirtualID[20];
             char receiverID[20];
+            int nextProc = ((index + 1) < numProc ? index + 1 : 0 );
 
             sprintf(childVirtualID, "%d", index);
-            sprintf(receiverID, "%d", ((index + 1) < numProc ? index + 1 : 0 )); //If the current process will be the
-            // last one, then make its receiver the original process
+            //If the current process will be the last one, then make its receiver the original process
+            sprintf(receiverID, "%d", nextProc);
 
             char file[] = "/home/kenny/CIS452/CIS452-Group1/Project1/child";
             //char file[] = "./child.out";
@@ -52,7 +74,7 @@ void StartChildren(int numProc) {//CLion needs a full path               // Poin
             args[3] = NULL;
 
             if( access( file, F_OK ) == 0 ) {
-                Execute(args);
+                childID[index] = Execute(args, index, nextProc);
             } else {
                 // file doesn't exist
                 printf("%s could not be found\n", file);
@@ -60,6 +82,14 @@ void StartChildren(int numProc) {//CLion needs a full path               // Poin
                 exit(1);
             }
     }
+    dup2(fd[0][READ], STDIN_FILENO); //Read on yours
+    dup2(fd[1][WRITE], STDOUT_FILENO); //Write on the next
+    close(fd[0][READ]);
+    close(fd[1][WRITE]);
+
+    fprintf(stdout, "can i print still? fprintf\n");
+    printf("can i print still? printf\n");
+    fputs("can i print still? fputs\n", stdout);
 }
 
 
@@ -67,9 +97,9 @@ void StartChildren(int numProc) {//CLion needs a full path               // Poin
  * Creates a child that runs the command using execvp.
  * @param command The parsed command
  */
-void Execute(char * command[])
+id_t Execute(char * command[], int procNum, int nextNum)
 {
-    id_t pid, child;
+    id_t pid;
     int status = 0;
 
     if ((pid = fork()) < 0) {
@@ -77,19 +107,27 @@ void Execute(char * command[])
         exit(1);
     }
     else if (pid == 0) {
+        dup2(fd[procNum][READ], 6); //Read on yours
+        dup2(fd[nextNum][WRITE], 5); //Write on the next
+        close(fd[procNum][READ]);
+        close(fd[nextNum][WRITE]);
+
+        fprintf(stdout, "can i print still? fprintf\n");
+        printf("can i print still? printf\n");
+        fputs("can i print still? fputs\n", stdout);
+
         if (execvp(command[0], command) < 0) {
             fprintf(stderr, "%s\n", strerror(errno));
             exit(1);
         }
     }
     else {
-        child = wait(&status);
+/*        child = wait(&status);
 
-        if(status > 0) printf("\nCommand failed to execute %d\n", status);
-
-        else {
-            printf("\nIt finally worked\n");
-        }
-        return;
+        if(status > 0) {
+            perror("\nCommand failed to execute\n");
+            exit(1);
+        }*/
+        return pid;
     }
 }
