3612a3a110fefe95c15fd1876355a5416a304fd1
George Ebeling <ebelingg@mail.gvsu.edu>
2021-04-07 12:57:56 -0400

    Removed a bunch of complicated stuff that can be done with Thread.sleep()

--- Project2/src/TimedProcess.java
+++ Project2/src/TimedProcess.java
@@ -2,13 +2,12 @@ import java.lang.reflect.*;
 import java.util.concurrent.*;
 
 public class TimedProcess {
-    TimedProcess(long millis) throws NoSuchMethodException {
+    private long MiliToLive;
+    TimedProcess(long millis) {
+        this.MiliToLive = millis;
 
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        String text = "tester";
-        Object phoneNumbers;
-        //This is supposed to start a timer that will call the CloseProcess() method when the timer finishes
-        service.schedule(new TimerHealthSupport(text, TimedProcess.class.getDeclaredMethod("CloseProcess", String.class)), millis, TimeUnit.MILLISECONDS);
+        // As long as we're multithreaded, I think we can use Thread.sleep(millis) to do the timer part of this.
+
         }
 
     private void CloseProcess(String name)
@@ -16,29 +15,7 @@ public class TimedProcess {
         System.out.println("Closing the processes (but not really)");
     }
 
-    // I found this on StackOverflow. I haven't tested it yet.
-    // https://stackoverflow.com/questions/43289895/how-to-trigger-events-in-java-at-specific-date-and-time
-    private class TimerHealthSupport implements Runnable {
-        private final String name;
-        private final Method close;
 
-        TimerHealthSupport(String name, Method closer)
-        {
-            this.name = name;
-            this.close = closer;
-        }
-
-        // This gets run when the timer runs out
-        public void run() {
-            try {
-                // This invoke is what should call CloseProcess
-                this.close.invoke(this.name);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                //IntelliJ made this for me
-                e.printStackTrace();
-            }
-        }
-    }
 }
 
 
--- Project2/src/main.java
+++ Project2/src/main.java
@@ -1,11 +1,21 @@
 /* This is a simple Java program.
    FileName : "HelloWorld.java". */
+
 class HelloWorld
 {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, World");
+
+        try {
+            var myProc = new TimedProcess(20);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            System.out.println("couldn't start process");
+    }
+        Thread.sleep(25);
+
     }
 }
